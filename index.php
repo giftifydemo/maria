127f501ce5a6311b181372a24dfec9db8de4c051
giftifydemo <112926630+giftifydemo@users.noreply.github.com>
2022-09-06 10:57:35 +0200

    nez one

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-echo "Hello Maria :) !<br>";
-for ($i=0; $i<100; $i+=10) {
-    echo "$i<br>";
-    }
+  $api_url = "https://v2.jokeapi.dev/joke/Any";
+  $joke = json_decode(file_get_contents($api_url));
+?>
+
+
+<?php echo $joke->category;?>
+
+      <?php if($joke->type=='single'){?>
+        <span><?php echo $joke->joke;?></span>
+      <?php } else{ ?>
+        <span><?php echo $joke->setup;?></span>
+        <hr>
+        <span><?php echo $joke->delivery;?></span>
+      <?php } ?>
